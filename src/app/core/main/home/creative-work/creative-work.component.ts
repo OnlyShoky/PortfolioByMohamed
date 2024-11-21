d5f70d5ffce69f56360258e8f36b985347ecfef4
OnlyShoky <mohamed.elmoag@gmail.com>
2024-11-21 15:30:50 +0100

    Removed grid, added flex

--- src/app/core/main/home/creative-work/creative-work.component.ts
+++ src/app/core/main/home/creative-work/creative-work.component.ts
@@ -21,34 +21,18 @@ export interface Tile {
 })
 export class CreativeWorkComponent implements OnInit {
   projects: Project[] = [];
-  tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1,  },
-    { text: 'Two', cols: 3, rows: 2,  },
-  ];
 
-  isBrowser: boolean;
+
 
   constructor(
     private profileDataService: ProfileDataService,
-    private router: Router,
-    @Inject(PLATFORM_ID) private platformId: Object
   ) {
-    this.isBrowser = isPlatformBrowser(this.platformId); // Check if running in the browser
-    if (this.isBrowser) {
-      this.projects = this.profileDataService.getProjects(3);
-    }
   }
 
-  ngOnInit() {
-    if (this.isBrowser) {
-      this.updateTileCols(window.innerWidth);
 
-      this.router.events.subscribe((event) => {
-        if (event instanceof NavigationEnd) {
-          window.scrollTo(0, 0); // Scroll to the top of the page
-        }
-      });
-    }
+  ngOnInit() {
+    this.projects = this.profileDataService.getProjects();
+
   }
 
   // Helper function to get technology details by name
@@ -56,38 +40,6 @@ export class CreativeWorkComponent implements OnInit {
     return this.profileDataService.getTechDetails(techName);
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    if (this.isBrowser) {
-      this.updateTileCols(window.innerWidth);
-    }
-  }
-
-  private updateTileCols(width: number) {
 
 
-
-      this.tiles[0].cols = 3;
-      this.tiles[0].rows = 1;
-      this.tiles[1].cols = 3;
-      this.tiles[1].rows = 3;
-
-
-    if (width < 1000) {
-      this.tiles[0].cols = 6;
-      this.tiles[0].rows = 1;
-      this.tiles[1].cols = 6;
-      this.tiles[1].rows = 4;
-    }
-      
-
-    if (width < 600) {
-      this.tiles[0].cols = 6;
-      this.tiles[0].rows = 2;
-      this.tiles[1].cols = 6;
-      this.tiles[1].rows = 7;
-
-  }
 }
-
-  }
